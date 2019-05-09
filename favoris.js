6139ff8af90234dd97a133e1c69deb70d803bc72
lea <lea.odienne@gmail.com>
2019-05-09 11:56:15 +0200

    hey

---
+++ favoris.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+     $('#coeur').on('click', fav);
+    
+});
+
+
+
+function fav(){
+	var url = window.location.href; 
+	var myUrl = url.split("=");
+	var userName = myUrl[1];
+    var productName = $(this).parents('div').attr('id');
+   
+
+    $.ajax({
+        url : 'favoris.php',
+        method : 'post',
+        dataType: 'json',
+        data : {userName:userName, productName:productName},
+        success:function(data){
+
+        }
+        });
+
+
+}
