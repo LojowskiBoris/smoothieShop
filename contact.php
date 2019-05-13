d10bcd9ca4385f6685ce1b2db13f86c48a244d27
LojowskiBoris <lojowski.boris@gmail.com>
2019-05-13 15:51:44 +0200

    yop

--- contact.php
+++ contact.php
@@ -10,18 +10,22 @@ include "layout.php";
 //         return $envoiFail = '<p>Erreur lors l\'envoi de votre message.</p>';
 //     }
 
-$message = $_POST['message'];
-$mail = $_POST['email'];
+if(!empty($_POST['message']) && !empty($_POST['email']) && array_key_exists('message',$_POST)  && array_key_exists('message',$_POST))
+{
+    $message = $_POST['message'];
+    $mail = $_POST['email'];
 
-if (isset($message)) {
-    $position_arobase = strpos($mail, '@');
-    if($position_arobase === false)
-        echo $mailIncorrect = '<p>Votre email est incorrect.</p>';
-    else {
-        $retour = mail('issam.elalaoui85@gmail.com','Envoi depuis le site SmoothieMaker', $message, 'From : ' . $mail);
-        if($retour)
-            return $envoiOk = '<p>Votre message a été envoyé.</p>';
-        else
-            return $envoiFail = '<p>Erreur lors l\'envoi de votre message.</p>';
+    if (isset($message)) {
+        $position_arobase = strpos($mail, '@');
+        if($position_arobase === false)
+            echo $mailIncorrect = '<p>Votre email est incorrect.</p>';
+        else {
+            $retour = mail('issam.elalaoui85@gmail.com','Envoi depuis le site SmoothieMaker', $message, 'From : ' . $mail);
+            if($retour)
+                return $envoiOk = '<p>Votre message a été envoyé.</p>';
+            else
+                return $envoiFail = '<p>Erreur lors l\'envoi de votre message.</p>';
+        }
     }
 }
+
