e60e2acd5c27bab77a6059787eb62ea90bb4e256
ibrainpop <ibrainpop@gmail.com>
2019-05-13 09:34:30 +0200

    ...

--- contact.php
+++ contact.php
@@ -1,3 +1,27 @@
 <?php
 
-include "contact.phtml";
+$template = "contact";
+include "layout.phtml";
+
+// $retour = mail('issam.elalaoui85@gmail.com', 'Envoi depuis le site SmoothieMaker', $_POST['message'], 'From : webmaster@smoothiesmaker.fr');
+//     if ($retour) {
+//         return $envoiOk = '<p>Votre message a été envoyé.</p>';
+//     }else{
+//         return $envoiFail = '<p>Erreur lors l\'envoi de votre message.</p>';
+//     }
+
+$message = $_POST['message'];
+$mail = $_POST['email'];
+
+if (isset($message)) {
+    $position_arobase = strpos($mail, '@');
+    if($position_arobase === false)
+        echo $mailIncorrect = '<p>Votre email est incorrect.</p>';
+    else {
+        $retour = mail('issam.elalaoui85@gmail.com','Envoi depuis le site SmoothieMaker', $message, 'From : ' . $mail);
+        if($retour)
+            return $envoiOk = '<p>Votre message a été envoyé.</p>';
+        else
+            return $envoiFail = '<p>Erreur lors l\'envoi de votre message.</p>';
+    }
+}
