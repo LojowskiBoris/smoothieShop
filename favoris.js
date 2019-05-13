2ab80352dcc9e2c785fb3258717c1404df1b57b5
Dynozo <urcel.d@gmail.com>
2019-05-13 14:50:26 +0200

    ajout Dy

--- favoris.js
+++ favoris.js
@@ -1,25 +1,28 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//      $('.coeur').on('click', fav);
+// });
 
-     $('#coeur').on('click', fav);
-    
-});
-
-function fav(){
-	var url = window.location.href; 
-	var myUrl = url.split("=");
-	var userName = myUrl[1];
-    var productName = $(this).parents('div').attr('id');
+// function fav(){
+// 	var url = window.location.href; 
+// 	var myUrl = url.split("=");
+// 	var userName = myUrl[1];
+//     var productName = $(this).parents('div').attr('id');
    
 
-    $.ajax({
-        url : 'favoris.php',
-        method : 'post',
-        dataType: 'json',
-        data : {productName:productName},
-        success:function(data){
+//     $.ajax({
+//         url : 'favoris.php',
+//         method : 'post',
+//         dataType: 'json',
+//         data : {productName:productName},
+//         success:function(data){
             
-        }
-        });
+//         }
+//         });
 
 
-}
+// }
+
+$('.coeur').on('click',function(){
+    $(this).toggleAttr('src','Ressources/assets/icons/fullheart.png');
+    },
+   );
