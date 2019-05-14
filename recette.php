f95768724c31991f6a3884ababe66dfb26478122
lea <lea.odienne@gmail.com>
2019-05-14 16:40:40 +0200

    voila

--- recette.php
+++ recette.php
@@ -1,5 +1,18 @@
 <?php
 include "bdd_connection.php";
+
+$requete = $pdo->prepare("
+SELECT * FROM `product`
+");
+
+$requete -> execute();
+
+$produits = $requete->fetchAll();
+
+// $i=0;
+
+
+
 $template = 'recette';
 include 'layout.php';
 
