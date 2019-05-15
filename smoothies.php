21775f9cbadc58cdad8b1a93e71e024b23c52626
lea <lea.odienne@gmail.com>
2019-05-15 09:28:12 +0200

    voilà

--- smoothies.php
+++ smoothies.php
@@ -1,4 +1,13 @@
 <?php
+include "bdd_connection.php";
+
+$requete = $pdo->prepare("
+SELECT * FROM `product`
+");
+
+$requete -> execute();
+
+$produits = $requete->fetchAll();
 
 $template = 'smoothies';
 include 'layout.php';
